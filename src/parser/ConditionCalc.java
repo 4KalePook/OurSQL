37bbbb67c8c71dc8425d775459a14d7927b01d31
RezaMash <rezaa.mash@gmail.com>
2015-11-13 15:32:21 +0330

    Debug and Some Temp functions

--- src/parser/ConditionCalc.java
+++ src/parser/ConditionCalc.java
@@ -6,7 +6,7 @@ public class ConditionCalc {
 	public ConditionCalc(Database database){
 		this.database = database;
 	}
-	public static boolean calculate(String tuple){//Calculate Tuple Condition
+	public  boolean calculate(String tuple){//Calculate Tuple Condition
 		tuple = Clean(tuple);
 		if(tuple.equals("FALSE"))
 			return false;
@@ -41,8 +41,19 @@ public class ConditionCalc {
 		String sub = Clean(tuple.substring(i,tuple.length()));
 		if(type == 0){
 			String value = getStrValue(ColName);
+			int compareFrom2 = value.compareTo(StrCompVal(sub.substring(2, sub.length() ) )) ;
+			int compareFrom1 = value.compareTo(StrCompVal(sub.substring(1, sub.length() ) )) ;
+			//TODO: compare < negative == 0 > positive ?
+			if(sub.startsWith("<=") || sub.startsWith("=<") )
+				return compareFrom2 <= 0;
+			if(sub.startsWith(">=") || sub.startsWith("=>") )
+				return compareFrom2 >= 0;
+			if(sub.startsWith(">") )
+				return compareFrom1 <  0;				
+			if(sub.startsWith(">") )
+				return compareFrom1 >  0;
 			if(sub.startsWith("="))
-				return value.equals( StrCompVal(sub.substring(2, sub.length() ) ) );
+				return compareFrom1 == 0;
 		}
 		if(type == 1){
 			int value = getIntValue(ColName);
@@ -52,18 +63,18 @@ public class ConditionCalc {
 			if(sub.startsWith(">=") || sub.startsWith("=>"))
 				return (value >= IntCompVal(sub.substring(2, sub.length())));
 			if(sub.startsWith(">") )
-				return (value > IntCompVal(sub.substring(2, sub.length())));
+				return (value > IntCompVal(sub.substring(1, sub.length())));
 			if(sub.startsWith("<"))
-				return (value < IntCompVal(sub.substring(2, sub.length())));
+				return (value < IntCompVal(sub.substring(1, sub.length())));
 			if(sub.startsWith("="))
-				return (value == IntCompVal(sub.substring(2, sub.length())));
+				return (value == IntCompVal(sub.substring(1, sub.length())));
 		}
 		//if we reach this point this means the string isn't a standard tuple condition
 		System.out.println("(Err)Not a standard Tupple Condition\n <"+tuple+">");
 		return false;
 	}
 	
-	private static boolean CalcIn(boolean A, String intuple){//Calculate inner Tuple Condition
+	private  boolean CalcIn(boolean A, String intuple){//Calculate inner Tuple Condition
 		intuple = Clean(intuple);
 		if(intuple.startsWith("AND"))
 			return (A && calculate(intuple.substring(3,intuple.length())));
@@ -74,7 +85,7 @@ public class ConditionCalc {
 		return false;
 	}
 	
-	private static String StrCompVal(String str){
+	private  String StrCompVal(String str){
 		//TODO: field of record
 		str = Clean(str);
 		if(str.charAt(0)== '"'){
@@ -87,12 +98,21 @@ public class ConditionCalc {
 			String sub2 = str.substring(i+1, str.length());
 			return InStrCompVal(sub1, sub2);
 		}
+		// if we reach this point this means the (int)compare starts with a field
+		int i = 0;
+		while(i<str.length()&& (is_digit(str.charAt(i))||is_letter(str.charAt(i))))
+			i++;
+		String value = getStrField(str.substring(0,i));
+		if(i==str.length())
+			return value;
+		if(i!=0)
+			return InStrCompVal(value, str.substring(i, str.length()));
 		
 		
 		//TODO: why reach here
 		return "";
 	}
-	private static String InStrCompVal(String str1, String str2){
+	private  String InStrCompVal(String str1, String str2){
 		str2 = Clean(str2);
 		if(str2.startsWith("+"))
 			return str1 + StrCompVal(str2.substring(1,str2.length()));
@@ -101,24 +121,34 @@ public class ConditionCalc {
 		return "";
 	}
 	
-	private static int IntCompVal(String comp){
+	private  int IntCompVal(String comp){
 		//TODO:field of record
 		int i = 0;
 		int numb = 0;
-		while(is_digit(comp.charAt(i))){
+		while(i<comp.length()&& is_digit(comp.charAt(i))){
 			numb*=10;
 			numb+=comp.charAt(i)-'0';
 			i++;
 		}
+		if(i==comp.length())
+			return numb;
 		if(i!=0)
 			return inIntCompVal(numb, comp.substring(i, comp.length()));
-		
+		// if we reach this point this means the (int)compare starts with a field
+		// i == 0
+		while(i<comp.length()&& (is_digit(comp.charAt(i))||is_letter(comp.charAt(i))))
+			i++;
+		int value = getIntField(comp.substring(0,i));
+		if(i==comp.length())
+			return value;
+		if(i!=0)
+			return inIntCompVal(value, comp.substring(i, comp.length()));
 		
 		//TODO: why reach here
 		return 0;
 	}
 	
-	private static int inIntCompVal(int numb, String incomp){
+	private  int inIntCompVal(int numb, String incomp){
 		incomp = Clean(incomp);
 		if(incomp.startsWith("+"))
 			return numb+IntCompVal(incomp.substring(1,incomp.length()));
@@ -133,7 +163,7 @@ public class ConditionCalc {
 		return 0;
 	}
 	
-	private static String Clean(String input){
+	private  String Clean(String input){
 		int i =0;
 		while(input.charAt(i)==' ')
 			i++;
@@ -145,26 +175,60 @@ public class ConditionCalc {
 		
 		return input.substring(i,j);
 	}
-	private static boolean is_letter(char c){
+	private  boolean is_letter(char c){
 		if('a'<=c && c<='z')
 			return true;
 		if('A'<=c && c<='Z')
 			return true;
 		return false;
 	}
-	private static boolean is_digit(char c){
+	private  boolean is_digit(char c){
+		//TODO: How?
 		if('0'<=c && c<='9')
 			return true;
 		return false;
 	}
-	private static int getIntValue(String a){
+	private  int getIntValue(String a){
+		//TODO: How?
+		if(a.equals("IK"))
+			return 3;
+		if(a.equals("IS"))
+			return 7;
 		return 0;
 	}
-	private static String getStrValue(String a){
+	private  String getStrValue(String a){
+		//TODO: How?
+		if(a.equals("SS"))
+			return "salam";
+		if(a.equals("SK"))
+			return "bye";
 		return "";
 	}
-	private static String getType(String a){
+	private  String getType(String a){
+		//TODO: How?
+		if(a.equals("IK")
+		|| a.equals("IS")
+		)
+			return "INT";
+		if(a.equals("SK")
+		|| a.equals("SS")
+		)
+			return "STRING";
 		return "STRING";
 		//return "INT";
+	}
+	private  int getIntField(String a){
+		if(a.equals("i5"))
+			return 5;
+		if(a.equals("i1"))
+			return 1;
+		return 0;
+	}
+	private  String getStrField(String a){
+		if(a.equals("sb"))
+			return "b";
+		if(a.equals("st"))
+			return "t";
+		return "";
 	}
 }
