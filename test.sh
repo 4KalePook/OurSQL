f565158aae0523d22cd60a889b8ce6f30002569a
FarzadAb <farzad.abdolhosseini@gmail.com>
2015-12-28 21:01:41 +0330

    added phase2 tests to tester

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
-FILES=./PPhase1-Test/PPhase1-Test/*
-for f in $FILES
+TESTS=('PPhase1-Test/PPhase1-Test' 'PPhase2-Test');
+for FOLDERS in "${TESTS[@]}";
 do
-echo "running on $f file..."
-java -jar OurSQL.jar < $f/in.txt > $f/myout.txt 2> $f/err.txt
-diff -sqBb $f/out.txt $f/myout.txt 
+	for f in ./$FOLDERS/*;
+	do
+		echo "running on $f file...";
+		java -jar OurSQL.jar < $f/in.txt > $f/myout.txt 2> $f/err.txt;
+		diff -sqBb $f/out.txt $f/myout.txt;
+	done
 done
